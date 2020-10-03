50ee6b01a009d3378c8a77d49499a9c13904b132
g2721 <37218380+g2721@users.noreply.github.com>
2020-10-03 12:30:02 +0200

    Update helloWorldWind.js

--- helloWorldWind.js
+++ helloWorldWind.js
@@ -65,23 +65,3 @@ colladaLoader.load("duck.dae", function (colladaModel) {
     colladaModel.scale = 9000;
     modelLayer.addRenderable(colladaModel);
 });
-
-// Add WMS imagery
-var serviceAddress = "https://neo.sci.gsfc.nasa.gov/wms/wms?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0";
-var layerName = "MOD_LSTD_CLIM_M";
-
-var createLayer = function (xmlDom) {
-    var wms = new WorldWind.WmsCapabilities(xmlDom);
-    var wmsLayerCapabilities = wms.getNamedLayer(layerName);
-    var wmsConfig = WorldWind.WmsLayer.formLayerConfiguration(wmsLayerCapabilities);
-    var wmsLayer = new WorldWind.WmsLayer(wmsConfig);
-    wwd.addLayer(wmsLayer);
-};
-
-var logError = function (jqXhr, text, exception) {
-    console.log("There was a failure retrieving the capabilities document: " +
-        text +
-    " exception: " + exception);
-};
-
-$.get(serviceAddress).done(createLayer).fail(logError);
