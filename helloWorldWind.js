7bdec9ad9c52ed64a3b0942436089f32631858be
g2721 <37218380+g2721@users.noreply.github.com>
2020-10-03 12:42:48 +0200

    Update helloWorldWind.js

--- helloWorldWind.js
+++ helloWorldWind.js
@@ -4,7 +4,7 @@ var wwd = new WorldWind.WorldWindow("canvasOne");
 wwd.addLayer(new WorldWind.BMNGOneImageLayer());
 wwd.addLayer(new WorldWind.BMNGLandsatLayer());
 
-wwd.addLayer(new WorldWind.CompassLayer());
+//wwd.addLayer(new WorldWind.CompassLayer());
 wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
 wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
 
