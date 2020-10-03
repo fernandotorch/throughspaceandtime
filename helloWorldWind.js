e8231b79d84889b8a78be80fb8bd144a6d57dccd
g2721 <37218380+g2721@users.noreply.github.com>
2020-10-03 12:43:32 +0200

    Update helloWorldWind.js

--- helloWorldWind.js
+++ helloWorldWind.js
@@ -8,31 +8,4 @@ wwd.addLayer(new WorldWind.BMNGLandsatLayer());
 wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
 wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
 
-// Add a placemark
-var placemarkLayer = new WorldWind.RenderableLayer("Placemark");
-wwd.addLayer(placemarkLayer);
-
-var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
-
-placemarkAttributes.imageOffset = new WorldWind.Offset(
-    WorldWind.OFFSET_FRACTION, 0.3,
-    WorldWind.OFFSET_FRACTION, 0.0);
-
-placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
-placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
-            WorldWind.OFFSET_FRACTION, 0.5,
-            WorldWind.OFFSET_FRACTION, 1.0);
-
-placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/pushpins/plain-red.png";
-
-var position = new WorldWind.Position(55.0, -106.0, 100.0);
-var placemark = new WorldWind.Placemark(position, true, placemarkAttributes);
-
-placemark.label = "Placemark\n" +
-    "Lat " + placemark.position.latitude.toPrecision(4).toString() + "\n" +
-    "Lon " + placemark.position.longitude.toPrecision(5).toString();
-placemark.alwaysOnTop = true;
-
-placemarkLayer.addRenderable(placemark);
-
 
